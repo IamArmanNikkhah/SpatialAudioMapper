9b22ddb700e871f78bb74cbdc29e5c010a35e196
Nik Khah <axn230007@utdallas.edu>
2024-02-24 16:41:09 -0600

    Modify run to save CSV file

--- gen_sph_power_map.py
+++ gen_sph_power_map.py
@@ -1,12 +1,13 @@
 import sys, argparse
 import numpy as np
+import csv
 from distance import SphericalAmbisonicsVisualizer, SphericalSourceVisualizer
 from audio import load_wav
 from video import VideoWriter
 from matplotlib import pyplot as plt
 
 
-def run(input_fn, output_fn, position_fn='', angular_res=''):
+def run(input_fn, output_fn, position_fn='', angular_res='', csv_output='frame_data.csv'):
     data, rate = load_wav(input_fn)
     duration = data.shape[0] / float(rate)
 
@@ -14,30 +15,34 @@ def run(input_fn, output_fn, position_fn='', angular_res=''):
     if position_fn:
         srcVis = SphericalSourceVisualizer(position_fn, duration, ambiVis.visualization_rate(), angular_res=angular_res)
 
-    # writer = VideoWriter(output_fn,
-    #                    video_fps=ambiVis.visualization_rate(),
-    #                    width=ambiVis.frame_dims[1],
-    #                    height=ambiVis.frame_dims[0],
-    #                    rgb=True)
-        
-    writer = VideoWriter(output_fn,
-                         video_fps=ambiVis.visualization_rate(), overwrite=False)
+    writer = VideoWriter(output_fn, video_fps=ambiVis.visualization_rate(), overwrite=True)
 
     cmap = np.stack(plt.get_cmap('inferno').colors)
-    while True:
-        frame = ambiVis.get_next_frame()
-        if frame is None:
-            break
-        frame /= frame.max()
+    
+    with open(csv_output, 'w', newline='') as csvfile:
+        
+        csvwriter = csv.writer(csvfile)
+        header = ['Pixel_' + str(i) for i in range(1, 19 * 36 + 1)]
+        csvwriter.writerow(header)
+        
+        while True:
+            frame = ambiVis.get_next_frame()
+            if frame is None:
+                break
+            
+            frame /= frame.max()
 
-        # Super-impose gt position
-        if position_fn:
-            frame += srcVis.get_next_frame()
+            if position_fn:
+                frame += srcVis.get_next_frame()
 
-        # Process frame and write to disk
-        frame = ((frame / frame.max()) * 255).astype(np.uint8)
-        frame = (cmap[frame] * 255).astype(np.uint8)    # Add colormap
-        writer.write_frame(frame)
+            # Normalize and apply colormap for video visualization
+            frame_video = ((frame / frame.max()) * 255).astype(np.uint8)
+            frame_video = (cmap[frame_video] * 255).astype(np.uint8)
+            writer.write_frame(frame_video)
+
+            # Flatten the frame to write into CSV
+            frame_flattened = frame.flatten()
+            csvwriter.writerow(frame_flattened)
 
 
 def parse_arguments():
